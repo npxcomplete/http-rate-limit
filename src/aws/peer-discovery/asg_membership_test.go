58bb7a0886fbbd80312994b142c63629d62f4250
npxcomplete <58501262+npxcomplete@users.noreply.github.com>
2025-08-03 22:35:21 +1000

    Clarify peer discovery tests with GIVEN/WHEN/THEN comments (#7)

--- src/aws/peer-discovery/asg_membership_test.go
+++ src/aws/peer-discovery/asg_membership_test.go
@@ -17,6 +17,7 @@ func TestIPsForLocalASG(t *testing.T) {
 	ctrl := gomock.NewController(t)
 	defer ctrl.Finish()
 
+	// GIVEN metadata reveals the instance ID and peers exist in the same Auto Scaling group
 	md := &clients.MockMetadataClient{Ctrl: ctrl}
 	md.GetMetadataWithContextFunc = func(ctx context.Context, path string) (string, error) {
 		return "i-self", nil
@@ -42,7 +43,10 @@ func TestIPsForLocalASG(t *testing.T) {
 
 	factory := clients.ClientPreBuilds{EC2Client: ec2Mock, AutoScalingClient: asg, MetadataClient: md}
 
+	// WHEN retrieving IPs for peers in the local Auto Scaling group
 	ips, err := IPsForLocalASG(context.Background(), factory)
+
+	// THEN all peer IPs are returned
 	assert.NoError(t, err)
 	assert.ElementsMatch(t, []string{"1.1.1.1", "2.2.2.2"}, ips)
 }
@@ -51,6 +55,7 @@ func TestInstancesForLocalASGError(t *testing.T) {
 	ctrl := gomock.NewController(t)
 	defer ctrl.Finish()
 
+	// GIVEN Auto Scaling instance lookup fails
 	md := &clients.MockMetadataClient{Ctrl: ctrl}
 	md.GetMetadataWithContextFunc = func(ctx context.Context, path string) (string, error) {
 		return "i-self", nil
@@ -62,6 +67,165 @@ func TestInstancesForLocalASGError(t *testing.T) {
 	}
 
 	factory := clients.ClientPreBuilds{AutoScalingClient: asg, MetadataClient: md}
+
+	// WHEN retrieving instances for the local Auto Scaling group
 	_, err := InstancesForLocalASG(context.Background(), factory)
+
+	// THEN an error is returned
+	assert.Error(t, err)
+}
+
+func TestInstancesForLocalASGMetadataError(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	// GIVEN the instance metadata service fails
+	md := &clients.MockMetadataClient{Ctrl: ctrl}
+	md.GetMetadataWithContextFunc = func(ctx context.Context, path string) (string, error) {
+		return "", assert.AnError
+	}
+
+	factory := clients.ClientPreBuilds{MetadataClient: md}
+
+	// WHEN retrieving instances for the local Auto Scaling group
+	_, err := InstancesForLocalASG(context.Background(), factory)
+
+	// THEN an error is returned
+	assert.Error(t, err)
+}
+
+func TestInstancesForLocalASGNoInstances(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	// GIVEN the Auto Scaling group has no instances
+	md := &clients.MockMetadataClient{Ctrl: ctrl}
+	md.GetMetadataWithContextFunc = func(ctx context.Context, path string) (string, error) {
+		return "i-self", nil
+	}
+
+	asg := &clients.MockAutoScalingClient{Ctrl: ctrl}
+	asg.DescribeAutoScalingInstancesWithContextFunc = func(ctx context.Context, input *autoscaling.DescribeAutoScalingInstancesInput, opts ...request.Option) (*autoscaling.DescribeAutoScalingInstancesOutput, error) {
+		return &autoscaling.DescribeAutoScalingInstancesOutput{}, nil
+	}
+
+	factory := clients.ClientPreBuilds{AutoScalingClient: asg, MetadataClient: md}
+
+	// WHEN retrieving instances for the local Auto Scaling group
+	inst, err := InstancesForLocalASG(context.Background(), factory)
+
+	// THEN an empty slice is returned without error
+	assert.NoError(t, err)
+	assert.Empty(t, inst)
+}
+
+func TestInstancesForLocalASGDescribeGroupsError(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	// GIVEN retrieving Auto Scaling groups fails
+	md := &clients.MockMetadataClient{Ctrl: ctrl}
+	md.GetMetadataWithContextFunc = func(ctx context.Context, path string) (string, error) {
+		return "i-self", nil
+	}
+
+	asg := &clients.MockAutoScalingClient{Ctrl: ctrl}
+	asg.DescribeAutoScalingInstancesWithContextFunc = func(ctx context.Context, input *autoscaling.DescribeAutoScalingInstancesInput, opts ...request.Option) (*autoscaling.DescribeAutoScalingInstancesOutput, error) {
+		return &autoscaling.DescribeAutoScalingInstancesOutput{AutoScalingInstances: []*autoscaling.InstanceDetails{{AutoScalingGroupName: aws.String("asg")}}}, nil
+	}
+	asg.DescribeAutoScalingGroupsWithContextFunc = func(ctx context.Context, input *autoscaling.DescribeAutoScalingGroupsInput, opts ...request.Option) (*autoscaling.DescribeAutoScalingGroupsOutput, error) {
+		return nil, assert.AnError
+	}
+
+	factory := clients.ClientPreBuilds{AutoScalingClient: asg, MetadataClient: md}
+
+	// WHEN retrieving instances for the local Auto Scaling group
+	_, err := InstancesForLocalASG(context.Background(), factory)
+
+	// THEN an error is returned
+	assert.Error(t, err)
+}
+
+func TestInstancesForLocalASGNoGroups(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	// GIVEN no Auto Scaling groups are found
+	md := &clients.MockMetadataClient{Ctrl: ctrl}
+	md.GetMetadataWithContextFunc = func(ctx context.Context, path string) (string, error) {
+		return "i-self", nil
+	}
+
+	asg := &clients.MockAutoScalingClient{Ctrl: ctrl}
+	asg.DescribeAutoScalingInstancesWithContextFunc = func(ctx context.Context, input *autoscaling.DescribeAutoScalingInstancesInput, opts ...request.Option) (*autoscaling.DescribeAutoScalingInstancesOutput, error) {
+		return &autoscaling.DescribeAutoScalingInstancesOutput{AutoScalingInstances: []*autoscaling.InstanceDetails{{AutoScalingGroupName: aws.String("asg")}}}, nil
+	}
+	asg.DescribeAutoScalingGroupsWithContextFunc = func(ctx context.Context, input *autoscaling.DescribeAutoScalingGroupsInput, opts ...request.Option) (*autoscaling.DescribeAutoScalingGroupsOutput, error) {
+		return &autoscaling.DescribeAutoScalingGroupsOutput{}, nil
+	}
+
+	factory := clients.ClientPreBuilds{AutoScalingClient: asg, MetadataClient: md}
+
+	// WHEN retrieving instances for the local Auto Scaling group
+	inst, err := InstancesForLocalASG(context.Background(), factory)
+
+	// THEN an empty slice is returned without error
+	assert.NoError(t, err)
+	assert.Empty(t, inst)
+}
+
+func TestInstancesForLocalASGDescribeInstancesError(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	// GIVEN EC2 instance description fails
+	md := &clients.MockMetadataClient{Ctrl: ctrl}
+	md.GetMetadataWithContextFunc = func(ctx context.Context, path string) (string, error) {
+		return "i-self", nil
+	}
+
+	asg := &clients.MockAutoScalingClient{Ctrl: ctrl}
+	asg.DescribeAutoScalingInstancesWithContextFunc = func(ctx context.Context, input *autoscaling.DescribeAutoScalingInstancesInput, opts ...request.Option) (*autoscaling.DescribeAutoScalingInstancesOutput, error) {
+		return &autoscaling.DescribeAutoScalingInstancesOutput{AutoScalingInstances: []*autoscaling.InstanceDetails{{AutoScalingGroupName: aws.String("asg")}}}, nil
+	}
+	asg.DescribeAutoScalingGroupsWithContextFunc = func(ctx context.Context, input *autoscaling.DescribeAutoScalingGroupsInput, opts ...request.Option) (*autoscaling.DescribeAutoScalingGroupsOutput, error) {
+		return &autoscaling.DescribeAutoScalingGroupsOutput{AutoScalingGroups: []*autoscaling.Group{{Instances: []*autoscaling.Instance{{InstanceId: aws.String("i-self")}}}}}, nil
+	}
+
+	ec2Mock := &clients.MockEC2Client{Ctrl: ctrl}
+	ec2Mock.DescribeInstancesPagesWithContextFunc = func(ctx context.Context, input *ec2.DescribeInstancesInput, fn func(*ec2.DescribeInstancesOutput, bool) bool) error {
+		return assert.AnError
+	}
+
+	factory := clients.ClientPreBuilds{EC2Client: ec2Mock, AutoScalingClient: asg, MetadataClient: md}
+
+	// WHEN retrieving instances for the local Auto Scaling group
+	_, err := InstancesForLocalASG(context.Background(), factory)
+
+	// THEN an error is returned
+	assert.Error(t, err)
+}
+
+func TestIPsForLocalASGError(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	// GIVEN Auto Scaling instance lookup fails
+	md := &clients.MockMetadataClient{Ctrl: ctrl}
+	md.GetMetadataWithContextFunc = func(ctx context.Context, path string) (string, error) {
+		return "i-self", nil
+	}
+
+	asg := &clients.MockAutoScalingClient{Ctrl: ctrl}
+	asg.DescribeAutoScalingInstancesWithContextFunc = func(ctx context.Context, input *autoscaling.DescribeAutoScalingInstancesInput, opts ...request.Option) (*autoscaling.DescribeAutoScalingInstancesOutput, error) {
+		return nil, assert.AnError
+	}
+
+	factory := clients.ClientPreBuilds{AutoScalingClient: asg, MetadataClient: md}
+
+	// WHEN retrieving peer IPs for the local Auto Scaling group
+	_, err := IPsForLocalASG(context.Background(), factory)
+
+	// THEN an error is returned
 	assert.Error(t, err)
 }
